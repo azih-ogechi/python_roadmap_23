cb6d76b13096f13fa8d868811dc947b7388037d4
azih-ogechi <azihogechi123@yahoo.com>
2023-04-22 13:32:48 +0100

    str assign

---
+++ Assignment/assignment_1.py
@@ -0,0 +1,11 @@
+#with, false, or, else, true, break, class, while, and, yield (Reserved words)
+var_name="books"
+#string methods
+print(len(var_name))
+print(var_name.index("k"))
+print(var_name.capitalize())
+print(var_name.casefold())
+print(var_name.isalpha())
+#integer methods
+
+
---
+++ assignments 1.py
@@ -0,0 +1,3 @@
+# 'false','with','while','except','finally','true','not','else','none','or'
+var_name ="girl's"
+print(len(var_name))
