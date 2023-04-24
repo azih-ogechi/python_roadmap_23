ecb26f734dc3def2137b3aeeb523a86622a4b174
azih-ogechi <azihogechi123@yahoo.com>
2023-04-24 08:38:13 +0100

    str_assign

--- Assignment/assignment_1.py
+++ Assignment/assignment_1.py
@@ -1,11 +1,16 @@
 #with, false, or, else, true, break, class, while, and, yield (Reserved words)
 var_name="books"
 #string methods
-print(len(var_name))
-print(var_name.index("k"))
-print(var_name.capitalize())
-print(var_name.casefold())
-print(var_name.isalpha())
+print(len(var_name))  #the len method returns the length of an object.
+print(var_name.index("k")) #the index method helps you to find the index position of an element.
+print(var_name.capitalize()) #the capitalize method returns a string with the very first letter capitalised.
+print(var_name.casefold()) #the casefold converts all characters the string into lowercase letters.
+print(var_name.isalpha()) #the isalpha tells you if a string is written in alphabets.
+print(var_name.isdigit()) #the isdigit checks if all the characters in a string are digit values or not.
+print(var_name.islower()) #the islower tells if your string is in lowercase or not.
 #integer methods
+b = 3
+a = 2
+print(isinstance (b,int)) #
 
 
