ed83b00c56cf63134ad1b6ac529f7cb36e94a817
azih-ogechi <azihogechi123@yahoo.com>
2023-04-24 13:47:56 +0100

    data con

--- assignments 1.py
+++
@@ -1,3 +0,0 @@
-# 'false','with','while','except','finally','true','not','else','none','or'
-var_name ="girl's"
-print(len(var_name))
---
+++ data conversion assignment_2.py
@@ -0,0 +1,8 @@
+#converting from float to boolean
+x = 5.0
+x = bool(x)
+print(type(x))
+print(x)
+
+mystr = "yourname"
+print(mystr[-1: :-1])
