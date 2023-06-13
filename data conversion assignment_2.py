e5ae2800225a806ce293ce6d996b45e67be64ebf
azih-ogechi <azihogechi123@yahoo.com>
2023-06-13 19:52:24 +0100

    tuple

--- data conversion assignment_2.py
+++ data conversion assignment_2.py
@@ -1,16 +1,36 @@
-#converting from float to boolean
-x = 5.0
-x = bool(x)
-print(type(x))
-print(x)
+# converting from float to boolean
+# x = 5.0
+# x = bool(x)
+# print(type(x))
+# print(x)
+#
+# mystr = "yourname"
+# print(mystr[-1: :-1])
+#
+# fullname = "azih margaret ogechi"
+# firstname = fullname[0:4]
+# print(firstname)
+# secondname = fullname[5:13]
+# print(secondname)
+# thirdname = fullname[14:]
+# print(thirdname)
 
-mystr = "yourname"
-print(mystr[-1: :-1])
+# s = 'python is fun'
+# print(ord(min(s)))
+# print(s.find('N'))
+# print(len(s[::]))
+# print(s.rindex('n'))
+#
+# r = "linkedin"
+# print(r)
+# print(r[4:-4])
 
-#fullname = "azih margaret ogechi"
-#firstname = fullname[0:4]
-#print(firstname)
-#secondname = fullname[5:13]
-#print(secondname)
-#thirdname = fullname[14:]
-#print(thirdname)
+my_list = []
+my_list.append("apple")
+my_list.append("orange")
+my_list.append("mango")
+my_list.append("guava")
+my_list.append("lemon")
+print(my_list)
+my_list.pop(-4)
+
